99dd25f7f6bb0834a0d41658928f4439bb3c6c04
Arnaldo Carvalho de Melo <acme@mandriva.com>
2006-10-25 01:04:26 -0300

    More output layout changes, this time for the elusive task_struct
    Signed-off-by: Arnaldo Carvalho de Melo <acme@mandriva.com>

--- pahole.c
+++ pahole.c
@@ -224,7 +224,7 @@ unsigned long class_member__print(struct class_member *self)
 				 "%s:%d;", self->name, self->bit_size);
 	}
 out:
-	printf("        %-21s %-21s /* %5d %5lu */\n",
+	printf("        %-26s %-21s /* %5d %5lu */\n",
 	       class_name, member_name_bf, self->offset, size);
 	return size;
 }
@@ -263,7 +263,7 @@ void class__print(struct class *self)
 	size_t last_size = 0;
 	int last_offset = -1;
 
-	printf("%51.51s /* offset size */\n", "");
+	printf("%56.56s /* offset size */\n", "");
 	printf("%s {\n", class__name(self, name, sizeof(name)));
 	list_for_each_entry(pos, &self->members, node) {
 		 if (sum > 0) {
