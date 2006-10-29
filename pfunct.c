3dfd3638689547dc41db707a1db0cfb348304789
Arnaldo Carvalho de Melo <acme@mandriva.com>
2006-10-29 00:41:48 -0300

    Add getopt support in pfunct, options implemented should be self explaining, except for --classes, that asks that all non-inlined functions that receive as one of its parameter a pointer to the specified class be printed, just the name if --verbose is not used, or its complete prototype and where it is in the source code:
    [acme@newtoy net-2.6]$ pfunct --verbose --class=inode fs/ext3/built-in.o
    /* /pub/scm/linux/kernel/git/acme/net-2.6/fs/ext3/balloc.c 606 */
    void ext3_free_blocks(handle_t * handle, struct inode * inode, ext3_fsblk_t block, long unsigned int count);
    
    /* /pub/scm/linux/kernel/git/acme/net-2.6/fs/ext3/balloc.c 1404 */
    ext3_fsblk_t ext3_new_blocks(handle_t * handle, struct inode * inode, ext3_fsblk_t goal, long unsigned int * count, int * errp);
    
    /* /pub/scm/linux/kernel/git/acme/net-2.6/fs/ext3/balloc.c 1668 */
    ext3_fsblk_t ext3_new_block(handle_t * handle, struct inode * inode, ext3_fsblk_t goal, int * errp);
    
    /* /pub/scm/linux/kernel/git/acme/net-2.6/fs/ext3/balloc.c 346 */
    void ext3_init_block_alloc_info(struct inode * inode);
    
    /* /pub/scm/linux/kernel/git/acme/net-2.6/fs/ext3/balloc.c 388 */
    void ext3_discard_reservation(struct inode * inode);
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@mandriva.com>

--- pfunct.c
+++ pfunct.c
@@ -7,34 +7,108 @@
   published by the Free Software Foundation.
 */
 
-#include <stdio.h>
 #include <dwarf.h>
+#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "classes.h"
 
+static int verbose;
+
+static struct option long_options[] = {
+	{ "class",	required_argument,	NULL, 'c' },
+	{ "verbose",	no_argument,		NULL, 'V' },
+	{ NULL, 0, NULL, 0, }
+};
+
+static void usage(void)
+{
+	fprintf(stderr,
+		"usage: pfunct [options] <file_name> {<function_name>}\n"
+		" where: \n",
+		"   -c, --class=<class>  functions that have <class> "
+					"pointer parameters\n"
+		"   -V, --verbose        be verbose\n");
+}
+
+static int class__has_parameter_of_type(struct class *self,
+					struct class *target)
+{
+	struct class_member *pos;
+
+	list_for_each_entry(pos, &self->members, node) {
+		struct class *class = classes__find_by_id(&pos->type);
+
+		if (class != NULL && class->tag == DW_TAG_pointer_type) {
+			class = classes__find_by_id(&class->type);
+			if (class != NULL &&
+			    class->id.offset == target->id.offset)
+				return 1;
+		}
+	}
+	return 0;
+}
+
+static int iterator(struct class *class, void *cookie)
+{
+	if (class->tag != DW_TAG_subprogram || class->inlined)
+		return 0;
+
+	if (class__has_parameter_of_type(class, cookie)) {
+		if (verbose)
+			class__print(class);
+		else
+			printf("%s\n", class->name);
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc == 0) {
-		puts("usage: "
-		     "pfunct <elf_file_with_debug_info> {<function_name>}");
-		return EXIT_FAILURE;
+	int option, option_index;
+	const char *file_name = NULL;
+	const char *class_name = NULL;
+	const char *function_name = NULL;
+
+	while ((option = getopt_long(argc, argv, "c:V",
+				     long_options, &option_index)) >= 0)
+		switch (option) {
+		case 'c': class_name = optarg;	    break;
+		case 'V': verbose    = 1;	    break;
+		default: usage();		    return EXIT_FAILURE;
+		}
+
+	if (optind < argc) {
+		switch (argc - optind) {
+		case 1:	 file_name = argv[optind++];	 break;
+		case 2:	 file_name = argv[optind++];
+			 function_name = argv[optind++]; break;
+		default: usage();			 return EXIT_FAILURE;
+		}
 	}
 
-	if (classes__load(argv[1]) != 0) {
+	if (classes__load(file_name) != 0) {
 		fprintf(stderr, "pfunct: couldn't load DWARF info from %s\n",
-		       argv[1]);
+			file_name);
 		return EXIT_FAILURE;
 	}
 
-	if (argc == 2)
+	if (class_name != NULL) {
+		struct class *class = classes__find_by_name(class_name);
+
+		if (class == NULL)
+			printf("class %s not found!\n", class_name);
+		else
+			classes__for_each(iterator, class);
+	} else if (function_name == NULL)
 		classes__print(DW_TAG_subprogram);
 	else {
-		struct class *class = classes__find_by_name(argv[2]);
+		struct class *class = classes__find_by_name(function_name);
 		if (class != NULL)
 			class__print(class);
 		else
-			printf("function %s not found!\n", argv[2]);
+			printf("function %s not found!\n", function_name);
 	}
 
 	return EXIT_SUCCESS;
