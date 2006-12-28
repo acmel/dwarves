ded72c80bde2acf2933758e7dbfee8f07979d7d7
Arnaldo Carvalho de Melo <acme@mandriva.com>
2006-12-28 14:03:42 -0200

    [DTAGNAMES]: 6th dwarf
    Just dump the tag names, simple utility to help rework classes abstractions to
    reduce memory footprint:
    
    [acme@newtoy pahole]$ dtagnames /pub/scm/linux/kernel/git/acme/OUTPUT/qemu/linux-2.6/vmlinux | sort | uniq -c | sort -nr
    size: 110832408
     207882 formal_parameter
     167305 pointer_type
     106945 structure_type
      82200 subroutine_type
      44130 typedef
      28372 array_type
       9121 const_type
       7940 union_type
       6572 base_type
       1690 enumeration_type
        894 volatile_type
    [acme@newtoy pahole]$
    
    Argh, the 'struct class' catchall uses way too many memory for non
    'structure_type' classes, work to do to reduce the current 105MB memory
    footprint for processing a qemu vmlinux.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@mandriva.com>

---
+++ dtagnames.c
@@ -0,0 +1,70 @@
+/* 
+  Copyright (C) 2006 Mandriva Conectiva S.A.
+  Copyright (C) 2006 Arnaldo Carvalho de Melo <acme@mandriva.com>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of version 2 of the GNU General Public License as
+  published by the Free Software Foundation.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#include "classes.h"
+
+static void print_malloc_stats(void)
+{
+	struct mallinfo m = mallinfo();
+
+	fprintf(stderr, "size: %u\n", m.uordblks);
+}
+
+static int class__tag_name(struct class *self, void *cookie)
+{
+	puts(dwarf_tag_name(self->tag.tag));
+	return 0;
+}
+
+static int cu__dump_class_tag_names(struct cu *self, void *cookie)
+{
+	cu__for_each_class(self, class__tag_name, NULL, NULL);
+	return 0;
+}
+
+static void cus__dump_class_tag_names(struct cus *self)
+{
+	cus__for_each_cu(self, cu__dump_class_tag_names, NULL, NULL);
+}
+
+static void usage(void)
+{
+	fprintf(stderr, "usage: dtagnames <filename>\n");
+}
+
+int main(int argc, char *argv[])
+{
+	struct cus *cus;
+	char *filename = argv[1];
+
+	if (argc != 2) {
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	cus = cus__new(NULL, NULL);
+	if (cus == NULL) {
+		fputs("dtagnames: insufficient memory\n", stderr);
+		return EXIT_FAILURE;
+	}
+
+	if (cus__load(cus, filename) != 0) {
+		fprintf(stderr, "dtagnames: couldn't load DWARF info from %s\n",
+		       filename);
+		return EXIT_FAILURE;
+	}
+
+	cus__dump_class_tag_names(cus);
+	print_malloc_stats();
+	return EXIT_SUCCESS;
+}
