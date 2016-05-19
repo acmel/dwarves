55d9b20dbaf6901a9b15d783ee8750fae3f621d1
Arnaldo Carvalho de Melo <acme@redhat.com>
2016-05-19 10:09:06 -0300

    dwarf_loader: Ignore DW_TAG_dwarf_procedure when processing functions
    We have no use for these location expressions yet...
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- dwarf_loader.c
+++ dwarf_loader.c
@@ -1485,6 +1485,12 @@ static int die__process_function(Dwarf_Die *die, struct ftype *ftype,
 			 * new tool.
 			 */
 			continue;
+		case DW_TAG_dwarf_procedure:
+			/*
+			 * Ignore it, just location expressions, that we have no use for (so far).
+			 */
+			continue;
+		case DW_TAG_GNU_template_parameter_pack:
 		case DW_TAG_template_type_parameter:
 		case DW_TAG_template_value_parameter:
 			/* FIXME: probably we'll have to attach this as a list of
