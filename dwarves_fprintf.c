acmel/dwarves

commit e975ff247aa8be98c0ce895febbaff3d94b9daf3

dwarves_fprintf: Print cacheline boundaries in multiple union members
In 'struct audit_context' we have an union that have member structs that
straddles cacheline boundaries, the existing logic was showing those
cacheline boundaries only for the first struct in the union where that
straddling took place, all the subsequent structs where straddling also
takes place were not showing it, the struct:

struct audit_context {
<SNIP>
	union {
		struct {
			int        nargs;                /*   824     4 */

			/* XXX 4 bytes hole, try to pack */

			/* --- cacheline 13 boundary (832 bytes) --- */
			long int   args[6];              /*   832    48 */
		} socketcall;                            /*   824    56 */
		struct {
			kuid_t     uid;                  /*   824     4 */
			kgid_t     gid;                  /*   828     4 */
			umode_t    mode;                 /*   832     2 */

			/* XXX 2 bytes hole, try to pack */

			u32        osid;                 /*   836     4 */
			int        has_perm;             /*   840     4 */
			uid_t      perm_uid;             /*   844     4 */
			gid_t      perm_gid;             /*   848     4 */
			umode_t    perm_mode;            /*   852     2 */

			/* XXX 2 bytes hole, try to pack */

			long unsigned int qbytes;        /*   856     8 */
		} ipc;                                   /*   824    40 */
		struct {
			mqd_t      mqdes;                /*   824     4 */

			/* XXX 4 bytes hole, try to pack */

			struct mq_attr mqstat;           /*   832    64 */
		} mq_getsetattr;                         /*   824    72 */
		struct {
			mqd_t      mqdes;                /*   824     4 */
			int        sigev_signo;          /*   828     4 */
		} mq_notify;                             /*   824     8 */
		struct {
			mqd_t      mqdes;                /*   824     4 */

			/* XXX 4 bytes hole, try to pack */

			size_t     msg_len;              /*   832     8 */
			unsigned int msg_prio;           /*   840     4 */

dwarves_fprintf.c
@@ -827,6 +827,7 @@ static size_t union__fprintf(struct type *type, const struct cu *cu,
 	size_t printed = 0;
 	int indent = conf->indent;
 	struct conf_fprintf uconf;
+	uint32_t initial_union_cacheline;
 
 	if (indent >= (int)sizeof(tabs))
 		indent = sizeof(tabs) - 1;
@@ -838,6 +839,12 @@ static size_t union__fprintf(struct type *type, const struct cu *cu,
 
 	uconf = *conf;
 	uconf.indent = indent + 1;
+	/*
+	 * Save the cacheline we're in, then, after each union member, get
+	 * back to it. Else we'll end up showing cacheline boundaries in
+	 * just the first of a multi struct union, for instance.
+	 */
+	initial_union_cacheline = *uconf.cachelinep;
 	type__for_each_member(type, pos) {
 		struct tag *type = cu__type(cu, pos->tag.type);
 
@@ -851,6 +858,7 @@ static size_t union__fprintf(struct type *type, const struct cu *cu,
 		printed += union_member__fprintf(pos, type, cu, &uconf, fp);
 		fputc('\n', fp);
 		++printed;
+		*uconf.cachelinep = initial_union_cacheline;
 	}
 
 	return printed + fprintf(fp, "%.*s}%s%s", indent, tabs,

commit 70ef8c7f07ffd18edf9dd052a2a8323e63ecdbe6

dwarves_fprintf: Set conf.cachelinep in union__fprintf() too
union__fprintf() unconditionally uses conf.cachelinep, assuming there is
where the current cacheline is being kept, but if we call
union__fprintf() from something other than __class__fprintf(), then that
pointer is NULL, fix that.

Reported-by: Eric Blake <eblake@redhat.com>
Tested-by: Eric Blake <eblake@redhat.com>
Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1633348
Fixes: e975ff247aa8 ("dwarves_fprintf: Print cacheline boundaries in multiple union members")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -828,6 +828,7 @@ static size_t union__fprintf(struct type *type, const struct cu *cu,
 	int indent = conf->indent;
 	struct conf_fprintf uconf;
 	uint32_t initial_union_cacheline;
+	int cacheline = 0; /* This will only be used if this is the outermost union */
 
 	if (indent >= (int)sizeof(tabs))
 		indent = sizeof(tabs) - 1;
@@ -839,6 +840,17 @@ static size_t union__fprintf(struct type *type, const struct cu *cu,
 
 	uconf = *conf;
 	uconf.indent = indent + 1;
+
+	/*
+	 * We may be called directly or from tag__fprintf, so keep sure
+	 * we keep track of the cacheline we're in.
+	 *
+	 * If we're being called from an outer structure, i.e. union within
+	 * struct, class or another union, then this will already have a
+	 * value and we'll continue to use it.
+	 */
+	if (uconf.cachelinep == NULL)
+                uconf.cachelinep = &cacheline;
 	/*
 	 * Save the cacheline we're in, then, after each union member, get
 	 * back to it. Else we'll end up showing cacheline boundaries in