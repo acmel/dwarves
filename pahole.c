acmel/dwarves

commit 31664d60ad410a68a9a5a4dde42b725fd206187a

pahole: Show tagged enums as well when no class is specified
The pahole tool initial goal was to show struct holes, which can't
happen with a first level of a tagged union (a union with a name),
so those only were displayed when part of a higher level struct.

Show the first level tagged enums as well, as this is useful when just
wanting to see its members.

E.g.:

  $ pahole ../build/v4.20-rc5/net/core/sock.o | grep ^union
  union fpregs_state {
  union irq_stack_union {
  union sigval {
  union __sifields {
  union thread_union {
  union kernfs_node_id {
  union flowi_uli {
  union ethtool_flow_union {
  union key_payload {
  union bpf_attr {
  union tcp_md5_addr {
  $

Suggested-by: Matthew Wilcox <willy@infradead.org>
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -279,7 +279,7 @@ static void print_classes(struct cu *cu)
 	uint16_t id;
 	struct class *pos;
 
-	cu__for_each_struct(cu, id, pos) {
+	cu__for_each_struct_or_union(cu, id, pos) {
 		bool existing_entry;
 		struct structure *str;
 

dwarves.h
@@ -268,6 +268,20 @@ static inline __pure bool cu__is_c_plus_plus(const struct cu *cu)
 		else
 
 /**
+ * cu__for_each_struct_or_union - iterate thru all the struct and union tags
+ * @cu: struct cu instance to iterate
+ * @pos: struct class iterator
+ * @id: uint16_t tag id
+ */
+#define cu__for_each_struct_or_union(cu, id, pos)			\
+	for (id = 1; id < cu->types_table.nr_entries; ++id)		\
+		if (!(pos = tag__class(cu->types_table.entries[id])) || \
+		    !(tag__is_struct(class__tag(pos)) || 		\
+		      tag__is_union(class__tag(pos))))			\
+			continue;					\
+		else
+
+/**
  * cu__for_each_function - iterate thru all the function tags
  * @cu: struct cu instance to iterate
  * @pos: struct function iterator

commit 3ffe5ba93b63de8730bb8dd37e322bd4f4884425

pahole: Do not apply 'struct class' filters to 'struct type'
Now unions are handled as well in pahole, so bail out before checking
'struct class' members for struct specific filters when handling
non-structs in class__filter()

Tested-by: Andrii Nakryiko <andriin@fb.com>
Fixes: 31664d60ad41 ("pahole: Show tagged enums as well when no class is specified")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -409,6 +409,13 @@ static struct class *class__filter(struct class *class, struct cu *cu,
 	     strncmp(decl_exclude_prefix, tag__decl_file(tag, cu),
 		     decl_exclude_prefix_len) == 0))
 		return NULL;
+	/*
+	 * The following only make sense for structs, i.e. 'struct class',
+	 * and as we can get here with a union, that is represented by a 'struct type',
+	 * bail out if we get here with an union
+	 */
+	if (!tag__is_struct(class__tag(class)))
+		return class;
 
 	if (tag->top_level)
 		class__find_holes(class);