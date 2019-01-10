acmel/dwarves

commit a2cdc6c2a0a37637d0f3561dd528b74d7f04333d

dutil: Adopt strstart() from the linux perf tools sources
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dutil.h
@@ -291,6 +291,16 @@ static inline bool strlist__empty(const struct strlist *slist)
 	return rb_first(&slist->entries) == NULL;
 }
 
+/**
+ * strstarts - does @str start with @prefix?
+ * @str: string to examine
+ * @prefix: prefix to look for.
+ */
+static inline bool strstarts(const char *str, const char *prefix)
+{
+	return strncmp(str, prefix, strlen(prefix)) == 0;
+}
+
 void *zalloc(const size_t size);
 
 Elf_Scn *elf_section_by_name(Elf *elf, GElf_Ehdr *ep,

commit 68b93e6858ae77431cdbb6b4adfe695d588b7902

dutil: Add missing string.h header include
Addressing this build warning:

  In file included from /home/acme/git/pahole/dutil.c:10:
  /home/acme/git/pahole/dutil.h: In function ‘strstarts’:
  /home/acme/git/pahole/dutil.h:301:9: warning: implicit declaration of function ‘strncmp’ [-Wimplicit-function-declaration]
    return strncmp(str, prefix, strlen(prefix)) == 0;
           ^~~~~~~
  /home/acme/git/pahole/dutil.h:301:30: warning: implicit declaration of function ‘strlen’ [-Wimplicit-function-declaration]
    return strncmp(str, prefix, strlen(prefix)) == 0;
                                ^~~~~~
  /home/acme/git/pahole/dutil.h:301:30: warning: incompatible implicit declaration of built-in function ‘strlen’
  /home/acme/git/pahole/dutil.h:301:30: note: include ‘<string.h>’ or provide a declaration of ‘strlen’
  /home/acme/git/pahole/dutil.h:20:1:
  +#include <string.h>

  /home/acme/git/pahole/dutil.h:301:30:
    return strncmp(str, prefix, strlen(prefix)) == 0;
                                ^~~~~~

Fixes: a2cdc6c2a0a3 ("dutil: Adopt strstart() from the linux perf tools sources")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dutil.h
@@ -13,6 +13,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 #include <elf.h>
 #include <gelf.h>
 #include <asm/bitsperlong.h>