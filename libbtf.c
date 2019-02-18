acmel/dwarves

commit fe4e1f799c55c08a88a81eb8548eb2aef5935e17

btf_elf: Rename btf_elf__free() to btf_elf__delete()
That is the idiom for free its members and then free itself, 'free' is
just to free its members.

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

libbtf.c
@@ -149,11 +149,11 @@ struct btf_elf *btf_elf__new(const char *filename, Elf *elf)
 	return btfe;
 
 errout:
-	btf_elf__free(btfe);
+	btf_elf__delete(btfe);
 	return NULL;
 }
 
-void btf_elf__free(struct btf_elf *btfe)
+void btf_elf__delete(struct btf_elf *btfe)
 {
 	if (!btfe)
 		return;

btf_encoder.c
@@ -190,7 +190,7 @@ int btf_encoder__encode()
 	int err;
 
 	err = btf_elf__encode(btfe, 0);
-	btf_elf__free(btfe);
+	btf_elf__delete(btfe);
 	btfe = NULL;
 
 	return err;
@@ -255,6 +255,6 @@ int cu__encode_btf(struct cu *cu, int verbose)
 
 out:
 	if (err)
-		btf_elf__free(btfe);
+		btf_elf__delete(btfe);
 	return err;
 }

btf_loader.c
@@ -537,7 +537,7 @@ static int cu__fixup_btf_bitfields(struct cu *cu, struct btf_elf *btfe)
 
 static void btf_elf__cu_delete(struct cu *cu)
 {
-	btf_elf__free(cu->priv);
+	btf_elf__delete(cu->priv);
 	cu->priv = NULL;
 }
 

libbtf.h
@@ -38,7 +38,7 @@ struct base_type;
 struct ftype;
 
 struct btf_elf *btf_elf__new(const char *filename, Elf *elf);
-void btf_elf__free(struct btf_elf *btf);
+void btf_elf__delete(struct btf_elf *btf);
 
 int32_t btf_elf__add_base_type(struct btf_elf *btf, const struct base_type *bt);
 int32_t btf_elf__add_ref_type(struct btf_elf *btf, uint16_t kind, uint32_t type,

commit 54106025cd1488fd7e1816f277f9406b08dd41d7

libbtf: Fixup temp filename to .btf, not .btfe
Bug introduced when renaming pahole's 'struct btf' to 'struct btf_elf'.

Fixes: fe4e1f799c55 ("btf_elf: Rename btf_elf__free() to btf_elf__delete()")
Signed-off-by: Andrii Nakryiko <andriin@fb.com>
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

libbtf.c
@@ -636,7 +636,7 @@ static int btf_elf__write(struct btf_elf *btfe)
 			llvm_objcopy = "llvm-objcopy";
 
 		/* Use objcopy to add a .BTF section */
-		snprintf(tmp_fn, sizeof(tmp_fn), "%s.btfe", btfe->filename);
+		snprintf(tmp_fn, sizeof(tmp_fn), "%s.btf", filename);
 		close(fd);
 		fd = creat(tmp_fn, S_IRUSR | S_IWUSR);
 		if (fd == -1) {