acmel/dwarves

commit 3ffe5ba93b63de8730bb8dd37e322bd4f4884425

pahole: Do not apply 'struct class' filters to 'struct type'
Now unions are handled as well in pahole, so bail out before checking
'struct class' members for struct specific filters when handling
non-structs in class__filter()

Tested-by: Andrii Nakryiko <andriin@fb.com>
Fixes: 31664d60ad41 ("pahole: Show tagged enums as well when no class is specified")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -409,6 +409,13 @@ static struct class *class__filter(struct class *class, struct cu *cu,
 	     strncmp(decl_exclude_prefix, tag__decl_file(tag, cu),
 		     decl_exclude_prefix_len) == 0))
 		return NULL;
+	/*
+	 * The following only make sense for structs, i.e. 'struct class',
+	 * and as we can get here with a union, that is represented by a 'struct type',
+	 * bail out if we get here with an union
+	 */
+	if (!tag__is_struct(class__tag(class)))
+		return class;
 
 	if (tag->top_level)
 		class__find_holes(class);

commit 69970fc77ec53effd0c1c715033d251500b828ca

pahole: Filter out unions when looking for packable structs
After we made -C apply for unions we had to stop calling
class__find_holes() on them, but forgot to also filter them out when
using --packable, which lead us to, in print_packable_info to access
class->priv for unions, as they were not filtered out, which made
it think that class->priv had the reordered cloned class, b00m.

Fix it by filtering out unions when doing --packable.

Fixes: 3ffe5ba93b63 ("pahole: Do not apply 'struct class' filters to 'struct type'")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -413,7 +413,7 @@ static struct class *class__filter(struct class *class, struct cu *cu,
 	 * bail out if we get here with an union
 	 */
 	if (!tag__is_struct(class__tag(class)))
-		return class;
+		return show_packable ? NULL : class;
 
 	if (tag->top_level)
 		class__find_holes(class);