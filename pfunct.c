9b2eadf97b44dff1298f4cb8ae98c179ce34d545
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-04-04 15:44:09 -0300

    pfunct: Follow const, restrict, volatile in --expand_types
    Take:
    
      int tcp_md5_hash_key(struct tcp_md5sig_pool *hp, const struct tcp_md5sig_key *key)
    
    We were not generating the 'struct tcp_md5sig_key' forward decl or
    struct definition, stopping at 'const', which made this uncompilable.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- pfunct.c
+++ pfunct.c
@@ -333,7 +333,7 @@ static int function__emit_type_definitions(struct function *func,
 		if (type == NULL)
 			continue;
 
-		if (type->tag == DW_TAG_pointer_type) {
+		if (type->tag == DW_TAG_pointer_type || tag__is_modifier(type)) {
 			type = cu__type(cu, type->type);
 			goto try_again;
 		}
