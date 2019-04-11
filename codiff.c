acmel/dwarves

commit 6b1e43f2c1acacd287e3266f85f31376faed90c9

codiff: When comparing against a file with just one CU don't bother finding by name
I.e. when we have two object files with debugging info, and one of them
jas just one CU, then compare all the CUs in the other file to this
unique CU.

Case in hand: encode BTF in a file, then the BTF info has everything in
just one "compile unit", so when looking at the types in the DWARF
originals, we should just compare its types to what is in the single BTF
"compile unit".

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

codiff.c
@@ -283,9 +283,17 @@ static void diff_struct(const struct cu *new_cu, struct class *structure,
 					 new_cu, diff);
 }
 
+static struct cu *cus__find_pair(struct cus *cus, const char *name)
+{
+	if (cus->nr_entries == 1)
+		return list_first_entry(&cus->cus, struct cu, node);
+
+	return cus__find_cu_by_name(cus, name);
+}
+
 static int cu_find_new_tags_iterator(struct cu *new_cu, void *old_cus)
 {
-	struct cu *old_cu = cus__find_cu_by_name(old_cus, new_cu->name);
+	struct cu *old_cu = cus__find_pair(old_cus, new_cu->name);
 
 	if (old_cu != NULL && cu__same_build_id(old_cu, new_cu))
 		return 0;
@@ -332,7 +340,7 @@ static int cu_find_new_tags_iterator(struct cu *new_cu, void *old_cus)
 
 static int cu_diff_iterator(struct cu *cu, void *new_cus)
 {
-	struct cu *new_cu = cus__find_cu_by_name(new_cus, cu->name);
+	struct cu *new_cu = cus__find_pair(new_cus, cu->name);
 
 	if (new_cu != NULL && cu__same_build_id(cu, new_cu))
 		return 0;

commit ac32e5e908bacd8243f6d5c6e09cd9d279c445ba

codiff: Fix comparision of multi-cu against single-cu files
When the first arg, the old object file has multiple compile units, i.e.
multiple objects that were then linked into one, and the second just one
.o, or equivalent, i.e. a .BTF file, then codiff shouldn't try to
find the types in the single CU in each of the old CUs.

Think about a .BTF file generated from a multi-CU DWARF binary, it will
contain all the types in all of the DWARF CUs, so if we go on trying to
find all the BTF files in each of the CUs, we'll fail.

It only makes sense to go on the DWARF CUs looking for the type on the
.BTF section and then compare them.

Fixes: 6b1e43f2c1ac ("codiff: When comparing against a file with just one CU don't bother finding by name")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

codiff.c
@@ -840,7 +840,8 @@ failure:
 	cus__for_each_cu(old_cus, cu_diff_iterator, new_cus, NULL);
 	cus__for_each_cu(new_cus, cu_find_new_tags_iterator, old_cus, NULL);
 	cus__for_each_cu(old_cus, cu_show_diffs_iterator, NULL, NULL);
-	cus__for_each_cu(new_cus, cu_show_diffs_iterator, (void *)1, NULL);
+	if (new_cus->nr_entries > 1)
+		cus__for_each_cu(new_cus, cu_show_diffs_iterator, (void *)1, NULL);
 
 	if (total_cus_changed > 1) {
 		if (show_function_diffs)