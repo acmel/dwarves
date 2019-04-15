acmel/dwarves

commit dc3d4419610332d694ed88e7ed9cbcd2d96b550c

core: Improve the natural alignment calculation
We need to take more than just arrays into account when figuring out the
natural alignment of struct members, looking recursively at types till
we get to basic types and pointers.

Before this patch the 'new' struct field in the 'v' union was considered
__packed__, when in fact it is not, as the natural alignment for the
'state_id' typedef is 4, so it can start at offset 36 (or 4 considering
just its container struct), see below:

  $ pahole -IC nfsd4_lock /home/acme/git/build/v5.1-rc4+/fs/nfsd/nfs4xdr.o
  /* Used at: /home/acme/git/linux/fs/nfsd/nfs4xdr.c */
  /* <1717a> /home/acme/git/linux/fs/nfsd/xdr4.h:156 */
  struct nfsd4_lock {
          u32                        lk_type;              /*     0     4 */
          u32                        lk_reclaim;           /*     4     4 */
          u64                        lk_offset;            /*     8     8 */
          u64                        lk_length;            /*    16     8 */
          u32                        lk_is_new;            /*    24     4 */

          /* XXX 4 bytes hole, try to pack */

          union {
                  struct {
                          u32        open_seqid;           /*    32     4 */
                          stateid_t  open_stateid;         /*    36    16 */
                          u32        lock_seqid;           /*    52     4 */
                          clientid_t clientid;             /*    56     8 */
                          /* --- cacheline 1 boundary (64 bytes) --- */
                          struct xdr_netobj owner;         /*    64    16 */
                  } __attribute__((__packed__)) new;       /*    32    48 */
                  struct {
                          stateid_t  lock_stateid;         /*    32    16 */
                          u32        lock_seqid;           /*    48     4 */
                  } __attribute__((__packed__)) old;       /*    32    20 */

dwarves.c
@@ -1291,17 +1291,49 @@ void class__find_holes(struct class *class)
 	class->holes_searched = true;
 }
 
-static size_t class_member__byte_size_for_alignment(struct class_member *member, const struct cu *cu)
+static size_t type__natural_alignment(struct type *type, const struct cu *cu);
+
+static size_t tag__natural_alignment(struct tag *tag, const struct cu *cu)
 {
-	struct tag *type = tag__strip_typedefs_and_modifiers(&member->tag, cu);
+	size_t natural_alignment = 1;
 
-	if (type->tag == DW_TAG_array_type) {
-		type = tag__strip_typedefs_and_modifiers(type, cu);
-
-		return tag__size(type, cu);
+	if (tag__is_pointer(tag)) {
+		natural_alignment = cu->addr_size;
+	} else if (tag->tag == DW_TAG_base_type) {
+		natural_alignment = base_type__size(tag);
+	} else if (tag__is_enumeration(tag)) {
+		natural_alignment = tag__type(tag)->size / 8;
+	} else if (tag__is_struct(tag) || tag__is_union(tag)) {
+		natural_alignment = type__natural_alignment(tag__type(tag), cu);
+	} else if (tag->tag == DW_TAG_array_type) {
+		tag = tag__strip_typedefs_and_modifiers(tag, cu);
+		natural_alignment = tag__natural_alignment(tag, cu);
 	}
 
-	return member->byte_size;
+	return natural_alignment;
+}
+
+static size_t type__natural_alignment(struct type *type, const struct cu *cu)
+{
+	struct class_member *member;
+
+	if (type->natural_alignment != 0)
+		return type->natural_alignment;
+
+	type__for_each_member(type, member) {
+		/* XXX for now just skip these */
+		if (member->tag.tag == DW_TAG_inheritance &&
+		    member->virtuality == DW_VIRTUALITY_virtual)
+			continue;
+
+		struct tag *member_type = tag__strip_typedefs_and_modifiers(&member->tag, cu);
+		size_t member_natural_alignment = tag__natural_alignment(member_type, cu);
+
+		if (type->natural_alignment < member_natural_alignment)
+			type->natural_alignment = member_natural_alignment;
+	}
+
+	return type->natural_alignment;
 }
 
 bool class__infer_packed_attributes(struct class *cls, const struct cu *cu)
@@ -1332,19 +1364,12 @@ bool class__infer_packed_attributes(struct class *cls, const struct cu *cu)
 		if (pos->is_static)
 			continue;
 
-		size_t byte_size = class_member__byte_size_for_alignment(pos, cu);
+		struct tag *member_type = tag__strip_typedefs_and_modifiers(&pos->tag, cu);
+		size_t natural_alignment = tag__natural_alignment(member_type, cu);
 
 		/* Always aligned: */
-		if (byte_size == sizeof(char))
+		if (natural_alignment == sizeof(char))
 			continue;
-
-		if (byte_size >= cu->addr_size) {
-			max_natural_alignment = cu->addr_size;
-			if ((pos->byte_offset % cu->addr_size) == 0)
-				continue;
-		}
-
-		uint16_t natural_alignment = __roundup_pow_of_two(byte_size);
 
 		if (max_natural_alignment < natural_alignment)
 			max_natural_alignment = natural_alignment;

dwarves.h
@@ -902,6 +902,7 @@ static __pure inline int tag__is_class_member(const struct tag *tag)
  * @nr_static_members: number of static DW_TAG_member entries
  * @nr_tags: number of tags
  * @alignment: DW_AT_alignement, zero if not present, gcc emits since circa 7.3.1
+ * @natural_alignment: For inferring __packed__, normally the widest scalar in it, recursively
  */
 struct type {
 	struct namespace namespace;
@@ -911,6 +912,7 @@ struct type {
 	uint16_t	 nr_static_members;
 	uint16_t	 nr_members;
 	uint32_t	 alignment;
+	uint16_t	 natural_alignment;
 	uint8_t		 declaration; /* only one bit used */
 	uint8_t		 definition_emitted:1;
 	uint8_t		 fwd_decl_emitted:1;

commit 8471736f3c6cc80445c1a0d6734c56d7cb47cba8

core: Cope with zero sized types when looking for natural alignment
We'll use it in a division, so make it 1, solving cases like:

  $ pahole examples/tcp.o -C u64_stats_sync
  struct u64_stats_sync {

	  /* size: 0, cachelines: 0, members: 0 */
  };
  $

  $ pahole -C bpf_prog_stats examples/tcp.o
  struct bpf_prog_stats {
  	u64                        cnt;                  /*     0     8 */
  	u64                        nsecs;                /*     8     8 */
  	struct u64_stats_sync syncp;                     /*    16     0 */

  	/* size: 16, cachelines: 1, members: 3 */
  	/* last cacheline: 16 bytes */
  };
  $

That were sefaulting.

Fixes: dc3d44196103 ("core: Improve the natural alignment calculation")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves.c
@@ -1310,7 +1310,17 @@ static size_t tag__natural_alignment(struct tag *tag, const struct cu *cu)
 		natural_alignment = tag__natural_alignment(tag, cu);
 	}
 
-	return natural_alignment;
+	/*
+	 * Cope with zero sized types, like:
+	 *
+	 *	struct u64_stats_sync {
+	 *	#if BITS_PER_LONG==32 && defined(CONFIG_SMP)
+	 *	seqcount_t      seq;
+	 *	#endif
+	 *	};
+	 *
+	 */
+	return natural_alignment ?: 1;
 }
 
 static size_t type__natural_alignment(struct type *type, const struct cu *cu)