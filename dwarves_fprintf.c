acmel/dwarves

commit cf459ca16fb22ff0542442512a3b5c7b7899d7b4

fprintf: Pretty print struct members that are pointers to nameless structs
I.e. to structs defined inside other structs, without a name:

Before:

  $ pahole -C parsed_partitions /home/acme/git/build/v5.1-rc4+/block/partitions/check.o
  struct parsed_partitions {
  	struct block_device *      bdev;                 /*     0     8 */
  	char                       name[32];             /*     8    32 */
  	struct  *                  parts;                /*    40     8 */
  	int                        next;                 /*    48     4 */
  	int                        limit;                /*    52     4 */
  	bool                       access_beyond_eod;    /*    56     1 */

  	/* XXX 7 bytes hole, try to pack */

  	/* --- cacheline 1 boundary (64 bytes) --- */
  	char *                     pp_buf;               /*    64     8 */

  	/* size: 72, cachelines: 2, members: 7 */
  	/* sum members: 65, holes: 1, sum holes: 7 */
  	/* last cacheline: 8 bytes */
  };
  $

After:

  $ pahole -C parsed_partitions /home/acme/git/build/v5.1-rc4+/block/partitions/check.o
  struct parsed_partitions {
  	struct block_device * bdev;                      /*     0     8 */
  	char                       name[32];             /*     8    32 */
  	struct {
  		sector_t           from;                 /*    40     8 */
  		sector_t           size;                 /*    48     8 */
  		int                flags;                /*    56     4 */
  		bool               has_info;             /*    60     1 */
  		struct partition_meta_info info;         /*    61   101 */
  	} * parts; /*    40     8 */
  	int                        next;                 /*    48     4 */
  	int                        limit;                /*    52     4 */
  	bool                       access_beyond_eod;    /*    56     1 */

  	/* XXX 7 bytes hole, try to pack */

  	/* --- cacheline 1 boundary (64 bytes) --- */
  	char *                     pp_buf;               /*    64     8 */

dwarves_fprintf.c
@@ -595,6 +595,7 @@ static size_t type__fprintf(struct tag *type, const struct cu *cu,
 {
 	char tbf[128];
 	char namebf[256];
+	char namebfptr[258];
 	struct type *ctype;
 	struct conf_fprintf tconf;
 	size_t printed = 0;
@@ -673,6 +674,7 @@ static size_t type__fprintf(struct tag *type, const struct cu *cu,
 
 	if (tag__is_struct(type) || tag__is_union(type) ||
 	    tag__is_enumeration(type)) {
+inner_struct:
 		tconf.type_spacing -= 8;
 		tconf.prefix	   = NULL;
 		tconf.suffix	   = name;
@@ -697,6 +699,13 @@ next_type:
 							  tconf.type_spacing,
 							  &tconf, fp);
 				break;
+			}
+			if (tag__is_struct(ptype) || tag__is_union(ptype) ||
+			    tag__is_enumeration(ptype)) {
+				snprintf(namebfptr, sizeof(namebfptr), "* %s", name);
+				name = namebfptr;
+				type = ptype;
+				goto inner_struct;
 			}
 		}
 		/* Fall Thru */

commit 68f261d8dfff761e94ed6b437045bd07ba40292b

fprintf: Fix recursively printing named structs in --expand_types
In cf459ca16fb2 ("fprintf: Pretty print struct members that are pointers
to nameless structs") we end up breaking 'pahole --expand_types/-E.

All we need there is to print nameless structs when they are referenced
by a pointer member in a struct, i.e. it is defined only there, inline.

Check if that struct is unnamed before emitting it, keeping that fix
while fixing the regression it caused for --expand_types.

Fixes: cf459ca16fb2 ("fprintf: Pretty print struct members that are pointers to nameless structs")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -700,8 +700,8 @@ next_type:
 							  &tconf, fp);
 				break;
 			}
-			if (tag__is_struct(ptype) || tag__is_union(ptype) ||
-			    tag__is_enumeration(ptype)) {
+			if ((tag__is_struct(ptype) || tag__is_union(ptype) ||
+			    tag__is_enumeration(ptype)) && type__name(tag__type(ptype), cu) == NULL) {
 				snprintf(namebfptr, sizeof(namebfptr), "* %s", name);
 				tconf.rel_offset = 1;
 				name = namebfptr;