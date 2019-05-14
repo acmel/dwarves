acmel/dwarves

commit 986a3b58a86984e80a7fec5234a3a4d814139aa6

fprintf: Only add bitfield forced paddings when alignment info available
I.e. BTF doesn't have DW_AT_alignment, so avoid emitting the bitfield
for padding at the end:

  $ pahole -F btf -C rseq examples/tcp.o
  struct rseq {
	  __u32                      cpu_id_start;         /*     0     4 */
	  __u32                      cpu_id;               /*     4     4 */
  	union {
  		__u64              ptr64;                /*     8     8 */
  		__u64              ptr;                  /*     8     8 */
  	} rseq_cs;                                       /*     8     8 */
  	__u32                      flags;                /*    16     4 */

  	/* Force padding: */
  	__u32                      :32;
  	__u32                      :32;
  	__u32                      :32;

  	/* size: 32, cachelines: 1, members: 4 */
  	/* padding: 12 */
  	/* last cacheline: 32 bytes */
  };
  $

After:

  $ pahole -F btf -C rseq examples/tcp.o
  struct rseq {
	__u32                      cpu_id_start;         /*     0     4 */
	__u32                      cpu_id;               /*     4     4 */
	union {
		__u64              ptr64;                /*     8     8 */
		__u64              ptr;                  /*     8     8 */
	} rseq_cs;                                       /*     8     8 */
	__u32                      flags;                /*    16     4 */

	/* size: 32, cachelines: 1, members: 4 */
	/* padding: 12 */
	/* last cacheline: 32 bytes */
  };
  $

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -1596,7 +1596,11 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 		last = pos;
 	}
 
-	if (class->padding != 0 && type->alignment == 0) {
+	/*
+	 * BTF doesn't have alignment info, for now use this infor from the loader
+	 * to avoid adding the forced bitfield paddings and have btfdiff happy.
+	 */
+	if (class->padding != 0 && type->alignment == 0 && conf->has_alignment_info) {
 		tag_pos = cu__type(cu, last->tag.type);
 		size = tag__size(tag_pos, cu);
 

commit 3ed9a67967cfbdb325bab07d93df2742080df17e

fprintf: Avoid null dereference with NULL configs
Adding support for suppress_packed and leaving out bitfield paddings at the end
of classes when no alignment info is available accidentally used conf instead
of cconf, where conf can be NULL, which causes cconf to have the default
fprintf config.

Fixes: 986a3b58a869 ("fprintf: Only add bitfield forced paddings when alignment info available")
Fixes: 9a4d71930467 ("fprintf: Allow suppressing the inferred __attribute__((__packed__))")
Signed-off-by: Timo Paulssen <timonator@perpetuum-immobile.de>
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -1619,8 +1619,8 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 	 * BTF doesn't have alignment info, for now use this infor from the loader
 	 * to avoid adding the forced bitfield paddings and have btfdiff happy.
 	 */
-	if (class->padding != 0 && type->alignment == 0 && conf->has_alignment_info &&
-	    !conf->suppress_force_paddings) {
+	if (class->padding != 0 && type->alignment == 0 && cconf.has_alignment_info &&
+	    !cconf.suppress_force_paddings) {
 		tag_pos = cu__type(cu, last->tag.type);
 		size = tag__size(tag_pos, cu);
 
@@ -1716,7 +1716,7 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 out:
 	printed += fprintf(fp, "%.*s}", indent, tabs);
 
-	if (class->is_packed && !conf->suppress_packed)
+	if (class->is_packed && !cconf.suppress_packed)
 		printed += fprintf(fp, " __attribute__((__packed__))");
 
 	if (cconf.suffix)