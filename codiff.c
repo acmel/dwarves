acmel/dwarves

commit 991c6a3ebb5d9df4cb634596df4216b32f8a32e3

dwarves: Rename cus__loadfl with cus__load_files
Also introducing cus__load, that load just one file.

The new cus__load_files routine now iterates thru the provided array
calling cus__load for each, and that in turn will try first dwarf__load,
and if that fail, i.e. if no DWARF info is found, call ctf__load.

This now allows loading DWARF _and_ CTF files at the same time. This
will be useful in the future when we, from DWARF generate CTF and at the
same time do a codiff, comparing the freshly generated CTF file with the
DWARF it came from.

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

codiff.c
@@ -811,8 +811,7 @@ failure:
 
 	/* If old_file is a character device, leave its cus empty */
 	if (!S_ISCHR(st.st_mode)) {
-		filenames[0] = old_filename;
-		err = cus__loadfl(old_cus, NULL, filenames);
+		err = cus__load(old_cus, NULL, old_filename);
 		if (err != 0) {
 			cus__print_error_msg("codiff", old_cus, old_filename, err);
 			return EXIT_FAILURE;
@@ -826,8 +825,7 @@ failure:
 
 	/* If old_file is a character device, leave its cus empty */
 	if (!S_ISCHR(st.st_mode)) {
-		filenames[0] = new_filename;
-		err = cus__loadfl(new_cus, NULL, filenames);
+		err = cus__load(new_cus, NULL, new_filename);
 		if (err != 0) {
 			cus__print_error_msg("codiff", new_cus, new_filename, err);
 			return EXIT_FAILURE;

ctf_loader.c
@@ -880,14 +880,14 @@ static int cus__fixup_ctf_bitfields(struct cus *self)
 }
 
 int ctf__load(struct cus *self, struct conf_load *conf __unused,
-	      char *filenames[])
+	      const char *filename)
 {
 	struct ctf_state state;
 	int wordsize;
 
 	memset(&state, 0, sizeof(state));
 
-	open_files(&state, filenames[0]);
+	open_files(&state, filename);
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
 		fprintf(stderr, "Cannot set libelf version.\n");

ctf_loader.h
@@ -11,6 +11,6 @@
 struct cus;
 struct conf_load;
 
-int ctf__load(struct cus *self, struct conf_load *conf, char *filenames[]);
+int ctf__load(struct cus *self, struct conf_load *conf, const char *filename);
 
 #endif /* _CTF_LOADER_H_ */

ctracer.c
@@ -933,8 +933,7 @@ failure:
          * If a filename was specified, for instance "vmlinux", load it too.
          */
 	if (filename != NULL) {
-		char *paths[2] = { filename, NULL };
-		err = cus__loadfl(methods_cus, NULL, paths);
+		err = cus__load(methods_cus, NULL, filename);
 		if (err != 0) {
 			cus__print_error_msg("ctracer", methods_cus, filename, err);
 			goto out;

dtagnames.c
@@ -49,7 +49,7 @@ int main(int argc __unused, char *argv[])
 		goto out;
 	}
 
-	err = cus__loadfl(cus, NULL, argv + 1);
+	err = cus__load_files(cus, NULL, argv + 1);
 	if (err != 0)
 		goto out;
 

dwarf_loader.c
@@ -1619,6 +1619,7 @@ static void cus__load_module(struct cus *self, struct conf_load *conf,
 struct process_dwflmod_parms {
 	struct cus	 *cus;
 	struct conf_load *conf;
+	uint32_t	 nr_dwarf_sections_found;
 };
 
 static int cus__process_dwflmod(Dwfl_Module *dwflmod,
@@ -1640,8 +1641,10 @@ static int cus__process_dwflmod(Dwfl_Module *dwflmod,
 	Dwarf_Addr dwbias;
 	Dwarf *dw = dwfl_module_getdwarf(dwflmod, &dwbias);
 
-	if (dw != NULL)
+	if (dw != NULL) {
+		++parms->nr_dwarf_sections_found;
 		cus__load_module(self, parms->conf, dwflmod, dw);
+	}
 	/*
 	 * XXX We will fall back to try finding other debugging
 	 * formats (CTF), so no point in telling this to the user
@@ -1687,33 +1690,28 @@ static int cus__process_file(struct cus *self, struct conf_load *conf, int fd,
 	struct process_dwflmod_parms parms = {
 		.cus  = self,
 		.conf = conf,
+		.nr_dwarf_sections_found = 0,
 	};
 
 	/* Process the one or more modules gleaned from this file. */
 	dwfl_getmodules(dwfl, cus__process_dwflmod, &parms, 0);
 	dwfl_end(dwfl);
-	return 0;
+	return parms.nr_dwarf_sections_found ? 0 : -1;
 }
 
-int dwarf__load(struct cus *self, struct conf_load *conf, char *filenames[])
+int dwarf__load(struct cus *self, struct conf_load *conf, const char *filename)
 {
-	int err = 0, i = 0;
+	int fd, err;
 
 	elf_version(EV_CURRENT);
 
-	while (filenames[i] != NULL) {
-		int fd = open(filenames[i], O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-		if (fd == -1) {
-			fprintf(stderr, "%s: couldn't open %s\n", __func__,
-				filenames[i]);
-			++i;
-			continue;
-		}
-		cus__process_file(self, conf, fd, filenames[i]);
-		close(fd);
-		++i;
-	}
+	if (fd == -1)
+		return -1;
+
+	err = cus__process_file(self, conf, fd, filename);
+	close(fd);
 
 	return err;
 }

dwarf_loader.h
@@ -11,6 +11,6 @@
 struct cus;
 struct conf_load;
 
-int dwarf__load(struct cus *self, struct conf_load *conf, char *filenames[]);
+int dwarf__load(struct cus *self, struct conf_load *conf, const char *filename);
 
 #endif /* _DWARF_LOADER_H_ */

dwarves.c
@@ -2585,8 +2585,7 @@ int cus__load_dir(struct cus *self, struct conf_load *conf,
 			if (err != 0)
 				break;
 		} else if (fnmatch(filename_mask, entry->d_name, 0) == 0) {
-			char *paths[2] = { pathname, NULL, };
-			err = cus__loadfl(self, conf, paths);
+			err = cus__load(self, conf, pathname);
 			if (err != 0)
 				break;
 		}
@@ -2599,22 +2598,34 @@ out:
 	return err;
 }
 
-int cus__loadfl(struct cus *self, struct conf_load *conf, char *filenames[])
+int cus__load(struct cus *self, struct conf_load *conf, char *filename)
 {
-	int err = dwarf__load(self, conf, filenames);
+	int err = dwarf__load(self, conf, filename);
 	/*
 	 * If dwarf__load fails, try ctf__load. Eventually we should just
 	 * register all the shared objects at some directory and ask them
 	 * for the magic types they support or just pass them the file,
 	 * unloading the shared object if it says its not the type they
 	 * support.
-	 * FIXME: for now we just check if no DWARF info was found
-	 * by looking at the list of CUs found:
 	 */
-	if (list_empty(&self->cus))
-		err = ctf__load(self, conf, filenames);
+	if (err != 0)
+		err = ctf__load(self, conf, filename);
 
 	return err;
+}
+
+int cus__load_files(struct cus *self, struct conf_load *conf,
+		    char *filenames[])
+{
+	int i = 0;
+
+	while (filenames[i] != NULL) {
+		if (cus__load(self, conf, filenames[i]))
+			return -i;
+		++i;
+	}
+
+	return 0;
 }
 
 void cus__print_error_msg(const char *progname, const struct cus *cus,

commit aefa9303818b368abee9e9fd22d97e10f5e7b3be

codiff: Remove unused filenames variable
I changed the logic at some point stopping the use of that variable, but
then forgot to remove it, do it now.

Addresses this coverity report entry:

  Error: COMPILER_WARNING: [#def3]
  dwarves-1.13/codiff.c: scope_hint: In function 'main'
  dwarves-1.13/codiff.c:776:8: warning: variable 'filenames' set but not used [-Wunused-but-set-variable]
  #  776 |  char *filenames[2];
  #      |        ^~~~~~~~~
  #  774|   	int remaining, err, rc = EXIT_FAILURE;
  #  775|   	char *old_filename, *new_filename;
  #  776|-> 	char *filenames[2];
  #  777|   	struct stat st;
  #  778|

Need to find how to make cmake show these warnings...

Reported-by: William Cohen <wcohen@redhat.com>
Fixes: 991c6a3ebb5d ("dwarves: Rename cus__loadfl with cus__load_files")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

codiff.c
@@ -773,7 +773,6 @@ int main(int argc, char *argv[])
 {
 	int remaining, err, rc = EXIT_FAILURE;
 	char *old_filename, *new_filename;
-	char *filenames[2];
 	struct stat st;
 
 	if (argp_parse(&codiff__argp, argc, argv, 0, &remaining, NULL) ||
@@ -811,8 +810,6 @@ failure:
 		fprintf(stderr, "codiff: %s (%s)\n", strerror(errno), old_filename);
 		goto out_cus_delete;
 	}
-
-	filenames[1] = NULL;
 
 	/* If old_file is a character device, leave its cus empty */
 	if (!S_ISCHR(st.st_mode)) {