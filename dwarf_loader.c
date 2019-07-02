acmel/dwarves

commit c7fa6a9a401e6c7d00e3bcb1dec829d9b923b1bd

dwarf loader: Catch some more template tags
Just warn about them not being supported yet.

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarf_loader.c
@@ -36,6 +36,31 @@ struct strings *strings;
 
 #define hashtags__fn(key) hash_64(key, HASHTAGS__BITS)
 
+static void __tag__print_not_supported(uint32_t tag, const char *func)
+{
+
+	static bool dwarf_tags_warned[DW_TAG_rvalue_reference_type];
+	static bool dwarf_gnu_tags_warned[DW_TAG_GNU_formal_parameter_pack - DW_TAG_MIPS_loop];
+
+	if (tag < DW_TAG_MIPS_loop) {
+		if (dwarf_tags_warned[tag])
+			return;
+		dwarf_tags_warned[tag] = true;
+	} else {
+		uint32_t t = tag - DW_TAG_MIPS_loop;
+
+		if (dwarf_gnu_tags_warned[t])
+			return;
+		dwarf_gnu_tags_warned[t] = true;
+	}
+
+	fprintf(stderr, "%s: tag not supported (%s)!\n", func,
+		dwarf_tag_name(tag));
+}
+
+#define tag__print_not_supported(tag) \
+	__tag__print_not_supported(tag, __func__)
+
 struct dwarf_tag {
 	struct hlist_node hash_node;
 	Dwarf_Off	 type;
@@ -1176,10 +1201,13 @@ static int die__process_class(Dwarf_Die *die, struct type *class,
 {
 	do {
 		switch (dwarf_tag(die)) {
+		case DW_TAG_GNU_template_template_param:
+		case DW_TAG_GNU_template_parameter_pack:
 		case DW_TAG_template_type_parameter:
 		case DW_TAG_template_value_parameter:
 			/* FIXME: probably we'll have to attach this as a list of
  			 * template parameters to use at class__fprintf time... */
+			tag__print_not_supported(dwarf_tag(die));
 			continue;
 		case DW_TAG_inheritance:
 		case DW_TAG_member: {
@@ -1386,6 +1414,7 @@ static int die__process_function(Dwarf_Die *die, struct ftype *ftype,
 			/* FIXME: probably we'll have to attach this as a list of
  			 * template parameters to use at class__fprintf time... 
  			 * See die__process_class */
+			tag__print_not_supported(dwarf_tag(die));
 			continue;
 		case DW_TAG_formal_parameter:
 			tag = die__create_new_parameter(die, ftype, lexblock, cu);

commit 24ced5be8a753799e745e16fc23a568219bd259c

dwarf_loader: Fix array overrun access
Fixing this coverity report:

  Error: OVERRUN (CWE-119): [#def7]
  dwarves-1.13/dwarf_loader.c:54: cond_at_most: Checking "tag < DW_TAG_MIPS_loop" implies that "tag" may be up to 16512 on the true branch.
  dwarves-1.13/dwarf_loader.c:55: overrun-local: Overrunning array "dwarf_tags_warned" of 66 bytes at byte offset 16512 using index "tag" (which evaluates to 16512).
  #   53|
  #   54|   	if (tag < DW_TAG_MIPS_loop) {
  #   55|-> 		if (dwarf_tags_warned[tag])
  #   56|   			return;
  #   57|   		dwarf_tags_warned[tag] = true;

Reported-by: William Cohen <wcohen@redhat.com>
Fixes: c7fa6a9a401e ("dwarf loader: Catch some more template tags")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarf_loader.c
@@ -43,24 +43,12 @@ bool no_bitfield_type_recode = true;
 
 static void __tag__print_not_supported(uint32_t tag, const char *func)
 {
-#ifdef STB_GNU_UNIQUE
-	static bool dwarf_tags_warned[DW_TAG_rvalue_reference_type];
-	static bool dwarf_gnu_tags_warned[DW_TAG_GNU_formal_parameter_pack - DW_TAG_MIPS_loop];
-#else
-	static bool dwarf_tags_warned[DW_TAG_shared_type];
-	static bool dwarf_gnu_tags_warned[DW_TAG_class_template - DW_TAG_MIPS_loop];
-#endif
+	static bool dwarf_tags_warned[DW_TAG_GNU_call_site_parameter + 64];
 
-	if (tag < DW_TAG_MIPS_loop) {
+	if (tag < sizeof(dwarf_tags_warned)) {
 		if (dwarf_tags_warned[tag])
 			return;
 		dwarf_tags_warned[tag] = true;
-	} else {
-		uint32_t t = tag - DW_TAG_MIPS_loop;
-
-		if (dwarf_gnu_tags_warned[t])
-			return;
-		dwarf_gnu_tags_warned[t] = true;
 	}
 
 	fprintf(stderr, "%s: tag not supported %#x (%s)!\n", func,