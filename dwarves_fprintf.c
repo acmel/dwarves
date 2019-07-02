acmel/dwarves

commit 28a3bc7addad511972a3f673955eeed8cc71ebe5

fprintf: Support packed enums
Check if the size is different than sizeof(int), which should be good
enough for now for both 64-bit and 32-bit targets.

Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -355,8 +355,19 @@ size_t enumeration__fprintf(const struct tag *tag, const struct cu *cu,
 		printed += fprintf(fp, "%.*s\t%s = %u,\n", indent, tabs,
 				   enumerator__name(pos, cu), pos->value);
 
-	return printed + fprintf(fp, "%.*s}%s%s", indent, tabs,
-				 conf->suffix ? " " : "", conf->suffix ?: "");
+	printed += fprintf(fp, "%.*s}", indent, tabs);
+
+	/*
+	 * XXX: find out how to precisely determine the max size for an
+	 * enumeration, use sizeof(int) for now.
+	 */
+	if (type->size / 8 != sizeof(int))
+		printed += fprintf(fp, " __attribute__((__packed__))", conf->suffix);
+
+	if (conf->suffix)
+		printed += fprintf(fp, " %s", conf->suffix);
+
+	return printed;
 }
 
 static const char *tag__prefix(const struct cu *cu, const uint32_t tag,

commit e95dacb704bfebf2604160d26f166735ceb96b4b

fprintf: Remove unused printf arg when printing enumerations
A cut'n'paste error, noticed by coverity:

  Error: PRINTF_ARGS: [#def16]
  dwarves-1.13/dwarves_fprintf.c:369: extra_argument: This argument was not used by the format string: "conf->suffix". [Note: The source code implementation of the function has been overridden by a builtin model.]
  #  367|   	 */
  #  368|   	if (type->size / 8 != sizeof(int))
  #  369|-> 		printed += fprintf(fp, " __attribute__((__packed__))", conf->suffix);
  #  370|
  #  371|   	if (conf->suffix)

Reported-by: William Cohen <wcohen@redhat.com>
Fixes: 28a3bc7addad ("fprintf: Support packed enums")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves_fprintf.c
@@ -366,7 +366,7 @@ size_t enumeration__fprintf(const struct tag *tag, const struct cu *cu,
 	 * enumeration, use sizeof(int) for now.
 	 */
 	if (type->size / 8 != sizeof(int))
-		printed += fprintf(fp, " __attribute__((__packed__))", conf->suffix);
+		printed += fprintf(fp, " %s", "__attribute__((__packed__))");
 
 	if (conf->suffix)
 		printed += fprintf(fp, " %s", conf->suffix);