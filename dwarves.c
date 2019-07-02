acmel/dwarves

commit 7fc7148be7685dfaa9622c6f9e8a49b657b8b560

core: Fix thinko in type__find_first_biggest_size_base_type_member
ctracer was segfaulting due to this problem.

Reported-by: Breno Leitão <leitao@linux.vnet.ibm.com>
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

dwarves.c
@@ -397,11 +397,12 @@ reevaluate:
 		case DW_TAG_const_type:
 		case DW_TAG_typedef:
 		case DW_TAG_volatile_type: {
-			struct tag *tag = type = cu__type(cu, tag->type);
+			struct tag *tag = cu__type(cu, type->type);
 			if (type == NULL) {
-				tag__id_not_found_fprintf(stderr, tag->type);
+				tag__id_not_found_fprintf(stderr, type->type);
 				continue;
 			}
+			type = tag;
 		}
 			goto reevaluate;
 		case DW_TAG_enumeration_type:

commit 5fdfd09a6bbea5b05cef5e8405f994e0e7a17e3a

dwarves: Fix check in type__find_first_biggest_size_base_type_member()
We stopped scrubbing type when looking for type->type by assigning the
result of cu__type(cu, type->type) to a temporary 'tag' variable but
continued to check the result of cu__type() looking at type... Check
'tag' instead.

Addresses these coverity report entries:

  Error: NULL_RETURNS (CWE-476): [#def9]
  dwarves-1.13/dwarves.c:333: returned_null: "cu__type" returns "NULL" (checked 54 out of 62 times).
  dwarves-1.13/dwarves.c:333: var_assigned: Assigning: "tag" = "NULL" return value from "cu__type".
  dwarves-1.13/dwarves.c:338: alias: Assigning: "type" = "tag". Both pointers are now "NULL".
  dwarves-1.13/dwarves.c:312: dereference: Dereferencing "type", which is known to be "NULL".
  dwarves-1.13/codiff.c:137: example_assign: Example 1: Assigning: "old_type" = return value from "cu__type(old_cu, old->tag.type)".
  dwarves-1.13/codiff.c:141: example_checked: Example 1 (cont.): "old_type" has its value checked in "old_type == NULL".
  dwarves-1.13/ctracer.c:356: example_assign: Example 2: Assigning: "type" = return value from "cu__type(cu, tag->type)".
  dwarves-1.13/ctracer.c:358: example_checked: Example 2 (cont.): "type" has its value checked in "type == NULL".
  dwarves-1.13/dwarves.c:914: example_assign: Example 3: Assigning: "type" = return value from "cu__type(cu, tag->type)".
  dwarves-1.13/dwarves.c:916: example_checked: Example 3 (cont.): "type" has its value checked in "type == NULL".
  dwarves-1.13/dwarves.c:941: example_assign: Example 4: Assigning: "tag" = return value from "cu__type(cu, var->ip.tag.type)".
  dwarves-1.13/dwarves.c:942: example_checked: Example 4 (cont.): "tag" has its value checked in "tag != NULL".
  dwarves-1.13/dwarves_emit.c:139: example_assign: Example 5: Assigning: "ptr_type" = return value from "cu__type(cu, type->type)".
  dwarves-1.13/dwarves_emit.c:141: example_checked: Example 5 (cont.): "ptr_type" has its value checked in "ptr_type == NULL".
  #  310|   		}
  #  311|   reevaluate:
  #  312|-> 		switch (type->tag) {
  #  313|   		case DW_TAG_base_type:
  #  314|

  Error: REVERSE_INULL (CWE-476): [#def10]
  dwarves-1.13/dwarves.c:312: deref_ptr: Directly dereferencing pointer "type".
  dwarves-1.13/dwarves.c:334: check_after_deref: Null-checking "type" suggests that it may be null, but it has already been dereferenced on all paths leading to the check.
  #  332|   		case DW_TAG_volatile_type: {
  #  333|   			struct tag *tag = cu__type(cu, type->type);
  #  334|-> 			if (type == NULL) {
  #  335|   				tag__id_not_found_fprintf(stderr, type->type);
  #  336|   				continue;

Looking at this last one shows the problem in detail, check for NULL to
then deref it, phew.

This is just used in 'pahole --show_first_biggest_size_base_type_member'
and when fixing up alignment in the --reorganize code tho.

Reported-by: William Cohen <wcohen@redhat.com>
Fixes: 7fc7148be768 ("core: Fix thinko in type__find_first_biggest_size_base_type_member")

dwarves.c
@@ -331,7 +331,7 @@ reevaluate:
 		case DW_TAG_rvalue_reference_type:
 		case DW_TAG_volatile_type: {
 			struct tag *tag = cu__type(cu, type->type);
-			if (type == NULL) {
+			if (tag == NULL) {
 				tag__id_not_found_fprintf(stderr, type->type);
 				continue;
 			}