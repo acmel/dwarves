d133569bd0f0b422c4053bb42c87f0965d06744e
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-07-16 17:21:37 -0300

    pahole: No need to read DW_AT_alignment when encoding BTF
    No need to read the DW_AT_alignment, not used in BTF encoding.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- pahole.c
+++ pahole.c
@@ -1298,6 +1298,7 @@ static error_t pahole__options_parser(int key, char *arg,
 		  detached_btf_filename = arg; // fallthru
 	case 'J': btf_encode = 1;
 		  conf_load.get_addr_info = true;
+		  conf_load.ignore_alignment_attr = true;
 		  no_bitfield_type_recode = true;	break;
 	case 'l': conf.show_first_biggest_size_base_type_member = 1;	break;
 	case 'M': conf.show_only_data_members = 1;	break;
