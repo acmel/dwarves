748141ed5ea3bb46601757b5155adfcf37dec1bd
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-07-19 17:43:23 -0300

    pahole: Ignore DW_TAG_inline_expansion when encoding BTF
    As it will not be used, so don't waste cycles/memory parsing them:
    
      $ grep "model name" /proc/cpuinfo
      model name	: Intel(R) Core(TM) i5-7500 CPU @ 3.40GHz
      model name	: Intel(R) Core(TM) i5-7500 CPU @ 3.40GHz
      model name	: Intel(R) Core(TM) i5-7500 CPU @ 3.40GHz
      model name	: Intel(R) Core(TM) i5-7500 CPU @ 3.40GHz
      $
    
    Before:
    
      $ perf stat -r5 pahole -j --btf_encode_detached=vmlinux-j.btf -F dwarf vmlinux
    
       Performance counter stats for 'pahole -j --btf_encode_detached=vmlinux-j.btf -F dwarf vmlinux' (5 runs):
    
               10,973.13 msec task-clock:u              #    2.906 CPUs utilized            ( +-  0.13% )
                       0      context-switches:u        #    0.000 /sec
                       0      cpu-migrations:u          #    0.000 /sec
                 793,927      page-faults:u             #   72.352 K/sec                    ( +-  0.00% )
          33,585,562,298      cycles:u                  #    3.061 GHz                      ( +-  0.17% )
          72,687,766,428      instructions:u            #    2.16  insn per cycle           ( +-  0.15% )
          17,198,056,478      branches:u                #    1.567 G/sec                    ( +-  0.16% )
             129,011,360      branch-misses:u           #    0.75% of all branches          ( +-  0.53% )
    
                  3.7760 +- 0.0158 seconds time elapsed  ( +-  0.42% )
    
      $
    
    After:
    
      $ perf stat -r5 pahole -j --btf_encode_detached=vmlinux-j.btf -F dwarf vmlinux
    
       Performance counter stats for 'pahole -j --btf_encode_detached=vmlinux-j.btf -F dwarf vmlinux' (5 runs):
    
               10,487.54 msec task-clock:u              #    2.855 CPUs utilized            ( +-  0.31% )
                       0      context-switches:u        #    0.000 /sec
                       0      cpu-migrations:u          #    0.000 /sec
                 762,431      page-faults:u             #   72.699 K/sec                    ( +-  0.00% )
          31,994,949,358      cycles:u                  #    3.051 GHz                      ( +-  0.09% )
          69,129,157,311      instructions:u            #    2.16  insn per cycle           ( +-  0.00% )
          16,359,974,001      branches:u                #    1.560 G/sec                    ( +-  0.00% )
             122,800,385      branch-misses:u           #    0.75% of all branches          ( +-  0.23% )
    
                 3.67286 +- 0.00917 seconds time elapsed  ( +-  0.25% )
    
      $
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- pahole.c
+++ pahole.c
@@ -1300,6 +1300,7 @@ static error_t pahole__options_parser(int key, char *arg,
 	case 'J': btf_encode = 1;
 		  conf_load.get_addr_info = true;
 		  conf_load.read_alignment_attr = false;
+		  conf_load.ignore_inline_expansions = true;
 		  no_bitfield_type_recode = true;	break;
 	case 'l': conf.show_first_biggest_size_base_type_member = 1;	break;
 	case 'M': conf.show_only_data_members = 1;	break;
