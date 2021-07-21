2f3cc4dcce0eda3857af341facd9865f23eff551
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-07-21 12:27:37 -0300

    hash: Remove unused hash_32(), hash_ptr()
    We're only using hash_64(), so ditch unused parts.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- hash.h
+++ hash.h
@@ -16,37 +16,9 @@
 
 #include <stdint.h>
 
-/* 2^31 + 2^29 - 2^25 + 2^22 - 2^19 - 2^16 + 1 */
-#define GOLDEN_RATIO_PRIME_32 0x9e370001UL
-/*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
-#define GOLDEN_RATIO_PRIME_64 0x9e37fffffffc0001UL
-
-#if __WORDSIZE == 32
-#define GOLDEN_RATIO_PRIME GOLDEN_RATIO_PRIME_32
-#define hash_long(val, bits) hash_32(val, bits)
-#elif __WORDSIZE == 64
-#define hash_long(val, bits) hash_64(val, bits)
-#define GOLDEN_RATIO_PRIME GOLDEN_RATIO_PRIME_64
-#else
-#error Wordsize not 32 or 64
-#endif
-
 static inline uint64_t hash_64(const uint64_t val, const unsigned int bits)
 {
 	return (val * 11400714819323198485LLU) >> (64 - bits);
 }
 
-static inline uint32_t hash_32(uint32_t val, unsigned int bits)
-{
-	/* On some cpus multiply is faster, on others gcc will do shifts */
-	uint32_t hash = val * GOLDEN_RATIO_PRIME_32;
-
-	/* High bits are more random, so use them. */
-	return hash >> (32 - bits);
-}
-
-static inline unsigned long hash_ptr(void *ptr, unsigned int bits)
-{
-	return hash_long((unsigned long)ptr, bits);
-}
 #endif /* _LINUX_HASH_H */
