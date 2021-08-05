6fd4377a0db1e8d34d36e167a1ae3cd37fed1cde
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-08-05 16:42:09 -0300

    dwarf_loader: Remove unused 'dcus' argument from cu__finalize()
    Not used at all, ditch it.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- dwarf_loader.c
+++ dwarf_loader.c
@@ -2470,7 +2470,7 @@ static int class_member__cache_byte_size(struct tag *tag, struct cu *cu,
 	return 0;
 }
 
-static int cu__finalize(struct cu *cu, struct dwarf_cu *dcu, struct conf_load *conf)
+static int cu__finalize(struct cu *cu, struct conf_load *conf)
 {
 	cu__for_all_tags(cu, class_member__cache_byte_size, conf);
 	if (conf && conf->steal) {
@@ -2483,7 +2483,7 @@ static int finalize_cu_immediately(struct cus *cus, struct cu *cu,
 				   struct dwarf_cu *dcu,
 				   struct conf_load *conf)
 {
-	int lsk = cu__finalize(cu, dcu, conf);
+	int lsk = cu__finalize(cu, conf);
 	switch (lsk) {
 	case LSK__DELETE:
 		cu__delete(cu);
@@ -2895,7 +2895,7 @@ static int cus__load_module(struct cus *cus, struct conf_load *conf,
 	}
 
 	if (type_cu != NULL) {
-		type_lsk = cu__finalize(type_cu, &type_dcu, conf);
+		type_lsk = cu__finalize(type_cu, conf);
 		if (type_lsk == LSK__KEEPIT) {
 			cus__add(cus, type_cu);
 		}
