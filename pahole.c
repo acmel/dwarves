acmel/dwarves

commit e3e5a4626c94d9c5b4c22a43d0fa5a58c41cfa94

pahole: Make sure the header is read only once
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -1685,6 +1685,7 @@ static struct tag *tag__real_type(struct tag *tag, struct cu **cup, void *instan
 struct type_instance {
 	struct type *type;
 	struct cu   *cu;
+	bool	    read_already;
 	char	    instance[0];
 };
 
@@ -1698,6 +1699,7 @@ static struct type_instance *type_instance__new(struct type *type, struct cu *cu
 	if (instance) {
 		instance->type = type;
 		instance->cu   = cu;
+		instance->read_already = false;
 	}
 
 	return instance;
@@ -1768,6 +1770,16 @@ out_free_member_name:
 	return base_type__value(&instance->instance[byte_offset], member->byte_size);
 }
 
+static int type__instance_read_once(struct type_instance *instance, FILE *fp)
+{
+ 	if (!instance || instance->read_already)
+		return 0;
+
+ 	instance->read_already = true;
+
+	return fread(instance->instance, instance->type->size, 1, stdin) != 1 ? -1 : 0;
+}
+
 /*
  * struct prototype - split arguments to a type
  *
@@ -1803,12 +1815,10 @@ static int tag__stdio_fprintf_value(struct tag *type, struct prototype *prototyp
 	if (instance == NULL)
 		return -ENOMEM;
 
-	if (header) {
-		if (fread(header->instance, header->type->size, 1, stdin) != 1) {
-			int err = --errno;
-			fprintf(stderr, "pahole: --header (%s) type not be read\n", conf.header_type);
-			return err;
-		}
+	if (type__instance_read_once(header, stdin)) {
+		int err = --errno;
+		fprintf(stderr, "pahole: --header (%s) type not be read\n", conf.header_type);
+		return err;
 	}
 
 	if (conf.range || prototype->range) {

commit 9e2ecb5a2e9e37f9ee5322f2bc78e6f13fcab34b

pahole: Fix signedness of ternary expression operator
To address this clang warning:

  /var/home/acme/git/pahole/pahole.c: In function ‘type__instance_read_once’:
  /var/home/acme/git/pahole/pahole.c:1933:78: warning: operand of ‘?:’ changes signedness from ‘int’ to ‘uint32_t’ {aka ‘unsigned int’} due to unsignedness of other operand [-Wsign-compare]
   1933 |         return fread(instance->instance, instance->type->size, 1, fp) != 1 ? -1 : instance->type->size;

Fixes: e3e5a4626c94d9c5 ("pahole: Make sure the header is read only once")
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

pahole.c
@@ -1930,7 +1930,7 @@ static int64_t type__instance_read_once(struct type_instance *instance, FILE *fp
 
  	instance->read_already = true;
 
-	return fread(instance->instance, instance->type->size, 1, fp) != 1 ? -1 : instance->type->size;
+	return fread(instance->instance, instance->type->size, 1, fp) != 1 ? -1 : (int64_t)instance->type->size;
 }
 
 /*