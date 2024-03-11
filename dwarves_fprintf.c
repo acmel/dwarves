acmel/dwarves

commit 47e83035c12c153b579db2e5fabd306a0d6f2d29

core: The padding in ancestor classes may be used in descendants
This appeared on the radar, in an expanded type:

class Spell_AdjustVital : public SpellActionType {
public:

        /* class SpellActionType {
        public:

               	void SpellActionType(class SpellActionType *, const class SpellActionType  &);

                int ()(void) * *   _vptr.SpellActionType;              /*   0  8 */
          	void SpellActionType(class SpellActionType *, int);

                virtual void ~SpellActionType(class SpellActionType *, int);

                virtual int Cast(class SpellActionType *, class CUnit &, const class SpellType  &, class CUnit * &, const Vec2i  &);

                virtual void Parse(class SpellActionType *, struct lua_State *, int, int);

             	const int          ModifyManaCaster;                   /*   8   4 */
                /* vtable has 2 entries: {
                   [2] = Cast((null)),
                   [3] = Parse((null)),
                } */    } <ancestor>; */ /*     0    16 */

        /* XXX last struct has 4 bytes of padding */
        /* XXX 65532 bytes hole, try to pack */
        void Spell_AdjustVital(class Spell_AdjustVital *);

        virtual int Cast(class Spell_AdjustVital *, class CUnit &, const class SpellType  &, class CUnit * &, const Vec2i  &);

        virtual void Parse(class Spell_AdjustVital *, lua_State *, int, int);

        int                        HP;                  /*    12   4 */
        int                        Mana;                /*    16   4 */
        int                        Shield;              /*    20   4 */
        int                        MaxMultiCast;        /*    24   4 */
    	virtual void ~Spell_AdjustVital(class Spell_AdjustVital *, int);

        /* vtable has 2 entries: {
           [2] = Cast((null)),
           [3] = Parse((null)),
        } */
	/* size: 32, cachelines: 1, members: 5 */
        /* sum members: 32, holes: 1, sum holes: 65532 */

dwarves_fprintf.c
@@ -1747,8 +1747,11 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 							   cconf.indent, tabs);
 				}
 			} else {
-				const ssize_t cc_last_size = ((ssize_t)pos->byte_offset -
-							      (ssize_t)last->byte_offset);
+				ssize_t cc_last_size = ((ssize_t)pos->byte_offset -
+							(ssize_t)last->byte_offset);
+				// Are we using the padding of an ancestor class (DW_TAG_inheritance)?
+				if (last->tag.tag == DW_TAG_inheritance && last->hole < 0)
+					cc_last_size += last->hole;
 
 				if (cc_last_size > 0 &&
 				   (size_t)cc_last_size < last_size) {
@@ -1782,6 +1785,11 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 		cconf.first_member = last == NULL;
 
 		size = pos->byte_size;
+		if (tag_pos->tag == DW_TAG_inheritance && pos->hole < 0) {
+			// Using the padding of an ancestor class
+			size += pos->hole;
+		}
+
 		printed += fprintf(fp, "%.*s", cconf.indent, tabs);
 		printed += struct_member__fprintf(pos, pos_type, cu, &cconf, fp);
 
@@ -1818,14 +1826,6 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 
 		fputc('\n', fp);
 		++printed;
-
-		/* XXX for now just skip these */
-		if (tag_pos->tag == DW_TAG_inheritance) {
-			// But do it for the sum_bytes, otherwised we'll get a BFA
-			// by missing the size of the ancestors.
-			sum_bytes += pos->byte_size;
-			goto next_member;
-		}
 #if 0
 		/*
  		 * This one was being skipped but caused problems with:
@@ -1839,7 +1839,7 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 		if (pos->bitfield_size) {
 			sum_bits += pos->bitfield_size;
 		} else {
-			sum_bytes += pos->byte_size;
+			sum_bytes += size;
 		}
 
 		if (last == NULL || /* First member */

dwarves.c
@@ -1528,7 +1528,7 @@ void class__find_holes(struct class *class)
 		}
 		if (bit_holes)
 			class->nr_bit_holes++;
-		if (byte_holes)
+		if (byte_holes > 0)
 			class->nr_holes++;
 
 		last = pos;

dwarves.h
@@ -1058,7 +1058,10 @@ static inline int function__inlined(const struct function *func)
  * @alignment - DW_AT_alignement, zero if not present, gcc emits since circa 7.3.1
  * @accessibility - DW_ACCESS_{public,protected,private}
  * @virtuality - DW_VIRTUALITY_{none,virtual,pure_virtual}
- * @hole - If there is a hole before the next one (or the end of the struct)
+ * @hole - If there is a hole before the next one (or the end of the struct).
+ * 	   A negative hole may happen when there is padding on an DW_TAG_inheritance,
+ * 	   i.e. in a ancestor type and the compiler puts the class member of the
+ * 	   derived class to use that padding.
  * @has_bit_offset: Don't recalcule this, it came from the debug info (DWARF5's DW_AT_data_bit_offset)
  */
 struct class_member {
@@ -1067,6 +1070,7 @@ struct class_member {
 	uint32_t	 bit_offset;
 	uint32_t	 bit_size;
 	uint32_t	 byte_offset;
+	int		 hole;
 	size_t		 byte_size;
 	int8_t		 bitfield_offset;
 	uint8_t		 bitfield_size;
@@ -1079,7 +1083,6 @@ struct class_member {
 	uint8_t		 has_bit_offset:1;
 	uint8_t		 accessibility:2;
 	uint8_t		 virtuality:2;
-	uint16_t	 hole;
 };
 
 void class_member__delete(struct class_member *member);

commit 4faf35b97c3427d1e2eebd6d7c6d900813d8c73c

fprintf: Remove bogus deduction of negative hole from size of previous field
In the previous fix for the usage of padding from ancestor classes the
negative hole was being deducted from the calculation of the previous
member/ancestor that is done purely using the offsets of the current and
previous fields, where it makes no sense to consider the hole, so revert
that specific hunk, with it the "Bitfield combined with next fields"
comment, which is wrong, is removed.

Before:

  $ pahole -C Spell_AdjustVital objects/spell_adjustvital.cpp.o
  class Spell_AdjustVital : public SpellActionType {
  public:

          /* class SpellActionType     <ancestor>; */      /*     0    16 */

          /* XXX last struct has 4 bytes of padding */
          void Spell_AdjustVital(class Spell_AdjustVital *);

          virtual int Cast(class Spell_AdjustVital *, class CUnit &, const class SpellType  &, class CUnit * &, const Vec2i  &);

          virtual void Parse(class Spell_AdjustVital *, lua_State *, int, int);

          /* Bitfield combined with next fields */

          int                        HP;                   /*    12     4 */
          int                        Mana;                 /*    16     4 */
          int                        Shield;               /*    20     4 */
          int                        MaxMultiCast;         /*    24     4 */
          virtual void ~Spell_AdjustVital(class Spell_AdjustVital *, int);

          /* vtable has 2 entries: {
             [2] = Cast((null)),
             [3] = Parse((null)),
          } */
          /* size: 32, cachelines: 1, members: 5 */
          /* padding: 4 */
          /* paddings: 1, sum paddings: 4 */
          /* last cacheline: 32 bytes */
  };

After:

  $ pahole -C Spell_AdjustVital objects/spell_adjustvital.cpp.o
  class Spell_AdjustVital : public SpellActionType {
  public:

          /* class SpellActionType     <ancestor>; */      /*     0    16 */

          /* XXX last struct has 4 bytes of padding */
          void Spell_AdjustVital(class Spell_AdjustVital *);

          virtual int Cast(class Spell_AdjustVital *, class CUnit &, const class SpellType  &, class CUnit * &, const Vec2i  &);

          virtual void Parse(class Spell_AdjustVital *, lua_State *, int, int);

          int                        HP;                   /*    12     4 */
          int                        Mana;                 /*    16     4 */
          int                        Shield;               /*    20     4 */
          int                        MaxMultiCast;         /*    24     4 */
          virtual void ~Spell_AdjustVital(class Spell_AdjustVital *, int);

          /* vtable has 2 entries: {
             [2] = Cast((null)),
             [3] = Parse((null)),
          } */
          /* size: 32, cachelines: 1, members: 5 */
          /* padding: 4 */
          /* paddings: 1, sum paddings: 4 */
          /* last cacheline: 32 bytes */
  };

  $

Reported-by: Matthias Schwarzott <zzam@gentoo.org>
Fixes: 47e83035c12c153b ("core: The padding in ancestor classes may be used in descendants")

dwarves_fprintf.c
@@ -1747,12 +1747,8 @@ static size_t __class__fprintf(struct class *class, const struct cu *cu,
 							   cconf.indent, tabs);
 				}
 			} else {
-				ssize_t cc_last_size = ((ssize_t)pos->byte_offset -
-							(ssize_t)last->byte_offset);
-				// Are we using the padding of an ancestor class (DW_TAG_inheritance)?
-				if (last->tag.tag == DW_TAG_inheritance && last->hole < 0)
-					cc_last_size += last->hole;
-
+				const ssize_t cc_last_size = ((ssize_t)pos->byte_offset -
+							      (ssize_t)last->byte_offset);
 				if (cc_last_size > 0 &&
 				   (size_t)cc_last_size < last_size) {
 					if (!cconf.suppress_comments) {