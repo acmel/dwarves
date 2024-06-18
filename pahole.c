f7e3f0942fedb9454a174020b65486116d4ee240
Arnaldo Carvalho de Melo <acme@redhat.com>
2024-06-18 11:44:01 -0300

    pahole: Bail out when not finding debug anywhere
    On a system where neither DWARF nor BTF can be found anywhere (in
    /sys/kernel/btf, installed debuginfo files, /lib/modules/, etc) we were
    segfaulting by passing NULL to access() when checking if the last
    argument was a file or a class name, but when calling just 'pahole' on
    such a system, there are no arguments, so we should just check if the
    "remaining" (non-existent) argument is NULL and state that no debug info
    was found and just bail out.
    
    Reported-by: Matthias Schwarzott <zzam@gentoo.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- pahole.c
+++ pahole.c
@@ -3806,6 +3806,12 @@ try_sole_arg_as_class_names:
 	if (err != 0) {
 		if (class_name == NULL && !btf_encode && !ctf_encode) {
 			class_name = argv[remaining];
+
+			if (class_name == NULL) {
+				fprintf(stderr, "pahole: couldn't find any debug information on this system.\n");
+				goto out_dwarves_exit;
+			}
+
 			if (access(class_name, R_OK) == 0) {
 				fprintf(stderr, "pahole: file '%s' has no %s type information.\n",
 						class_name, conf_load.format_path ?: "supported");
